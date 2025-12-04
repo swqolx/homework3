4868e93eefaa32f1a5280818f277c638087c217e
ойойой <153764765+swqolx@users.noreply.github.com>
2025-12-04 18:30:17 +0200

    end

---
+++ hw3.js
@@ -0,0 +1,33 @@
+//==homework
+
+
+//===exercise 1
+
+// const result= 5+5+"5"
+// console.log(typeof(result));
+
+
+
+//===exercise 2
+
+// const email = "nastamch1294@gmail.com"
+// console.log(email.includes("@"));
+// console.log(email.length);
+
+
+//===exercise 3
+
+
+// const word1 = "My "
+// const word2 = "name "
+// const word3 = "is "
+// const fullName = word1 + word2 + word3 + "Victor"
+// console.log(fullName);
+
+
+
+//====exercise 4
+
+// const userName = "Alex"
+// const payment = "300"
+// console.log(`Дякуємо, ${userName}! До сплати ${payment} гривень`);
